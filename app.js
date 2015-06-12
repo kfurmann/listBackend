2e134a3b23c5f1ed1fbc2145d687935f661b5ecb
Krzysztof Furmann <krzysztof.furmann@gmail.com>
2015-06-12 12:46:20 +0200

    added sign in, find users by name

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var User = require('./models/User');
+
 var app = express();
+
+mongoose.connect('mongodb://localhost/local');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,19 +23,86 @@ app.set('view engine', 'jade');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
+
+app.post('/signin', function (req, res) {
+
+    function saveNewUser() {
+        var newUser = new User();
+        newUser.name = req.body.name;
+        newUser.save(function (err, user) {
+            if (err) {
+                res.json({
+                    type: false,
+                    data: "error " + err
+                })
+            } else {
+                res.json({
+                    type: true,
+                    data: "new user saved"
+                })
+            }
+        });
+    }
+
+    User.findOne({name: req.body.name}, function (err, user) {
+        if (err) {
+            // error
+            res.json({
+                type: false,
+                data: "error " + err
+            })
+        } else {
+            if (user) {
+                res.json({
+                    type: false,
+                    data: "User already exists!"
+                });
+            } else {
+                saveNewUser();
+            }
+        }
+    });
+});
+
+app.post('/findUsersByName', function (req, res) {
+
+    var re = new RegExp(req.body.name, 'i');
+
+    User.find({"name": {$regex: re}}, function (err, users) {
+        if (err) {
+            // error
+            res.json({
+                type: false,
+                data: "error " + err
+            })
+        } else {
+            res.json({
+                type: true,
+                data: users
+            });
+        }
+    })
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -38,23 +110,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
---
+++ models/User.js
@@ -0,0 +1,12 @@
+/**
+ * Created by kihu on 2015-06-11.
+ */
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+    name: String
+    //token: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
+
